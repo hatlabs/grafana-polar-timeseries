6a289c62c1ffd58bbfaa5b986bc8b597b7c523df
Matti Airas <mairas@iki.fi>
2021-08-11 14:07:17 +0300

    Remove commented out code

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
-//import { css, cx } from 'emotion';
-//import { stylesFactory, useTheme } from '@grafana/ui';
 import Plot from 'react-plotly.js';
 
 interface Props extends PanelProps<SimpleOptions> { }
 
-  //const theme = useTheme();
-  //const styles = getStyles();
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height, timeRange }) => {
   
   const plotData: Plotly.Data[] = data.series.map(item => {
@@ -53,59 +49,5 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height, tim
       layout={plotLayout}
       config={plotConfig}
     />
-    // <div
-    //   className={cx(
-    //     styles.wrapper,
-    //     css`
-    //       width: ${width}px;
-    //       height: ${height}px;
-    //     `
-    //   )}
-    // >
-    //   <svg
-    //     className={styles.svg}
-    //     width={width}
-    //     height={height}
-    //     xmlns="http://www.w3.org/2000/svg"
-    //     xmlnsXlink="http://www.w3.org/1999/xlink"
-    //     viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-    //   >
-    //     <g>
-    //       <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-    //     </g>
-    //   </svg>
-
-    //   <div className={styles.textBox}>
-    //     {options.showSeriesCount && (
-    //       <div
-    //         className={css`
-    //           font-size: ${theme.typography.size[options.seriesCountSize]};
-    //         `}
-    //       >
-    //         Number of series: {data.series.length}
-    //       </div>
-    //     )}
-    //     <div>Text option value: {options.text}</div>
-    //   </div>
-    // </div>
   );
 };
-
-// const getStyles = stylesFactory(() => {
-// return {
-// wrapper: css`
-// position: relative;
-// `,
-// svg: css`
-// position: absolute;
-// top: 0;
-// left: 0;
-// `,
-// textBox: css`
-// position: absolute;
-// bottom: 0;
-// left: 0;
-// padding: 10px;
-// `,
-// };
-//});
